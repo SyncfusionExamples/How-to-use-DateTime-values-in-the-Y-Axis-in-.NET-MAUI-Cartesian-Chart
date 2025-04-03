b666bbba1f006c24802a48a9bb6fdcb7dd510999
subash_s <subash.s@syncfusion.com>
2025-04-03 12:21:39 +0530

    sample included

---
+++ DateTimeY/DateTimeY/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.Maui.Toolkit.Charts;
+
+namespace DateTimeY
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NumericalAxis_LabelCreated(object sender, ChartAxisLabelEventArgs e)
+        {
+            if (double.TryParse(e.Label, out double minutes))
+            {
+                TimeSpan time = TimeSpan.FromMinutes(minutes);
+                e.Label = new DateTime(1, 1, 1, time.Hours, time.Minutes, 0).ToString("hh:mm tt");
+            }
+        }
+    }
+}
---
+++ DateTimeY/DateTimeY/ViewModel/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace DateTimeY
+{
+    public class ViewModel
+    {
+        public ObservableCollection<DataModel> SleepData { get; set; }
+
+        public ViewModel()
+        {
+            SleepData = new ObservableCollection<DataModel>()
+            {
+                new() { Day = "Monday", Duration = new DateTime(2025, 2, 10, 7, 30, 0) },
+                new() { Day = "Tuesday", Duration = new DateTime(2025, 2, 11, 6, 45, 0) },
+                new() { Day = "Wednesday", Duration = new DateTime(2025, 2, 12, 8, 0, 0) },
+                new() { Day = "Thursday", Duration = new DateTime(2025, 2, 13, 5, 50, 0) },
+                new() { Day = "Friday", Duration = new DateTime(2025, 2, 14, 7, 15, 0) },
+                new() { Day = "Saturday", Duration = new DateTime(2025, 2, 15, 9, 0, 0) },
+                new() { Day = "Sunday", Duration = new DateTime(2025, 2, 16, 8, 30, 0) }
+            };
+        }
+    }
+}
